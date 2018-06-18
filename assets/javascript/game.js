44d3772e679cf785de35fac6c59441aaca245ea7
Marshall Johnston <marshalljohnston77@gmail.com>
2018-06-18 09:55:17 -0400

    2nd iteration

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,16 @@
 // this is my object containing all the questions for the game
 var correct = 0; 
 var incorrect = 0; 
- 
+
+var missionResult = [{
+    win: "assets/images/missionaccomplished.png",
+    lose: "assets/images/missionfailed.png"
+
+}];
 
 var questionsList = [{
 
-    question: "We received a tip from a secret source that she was in Burkina Faso, make your way to the capital city.", 
+    question: "We received a tip from a secret source that 'X' was in Burkina Faso, make your way to the capital city.", 
     answers: ["Ouagadougou", "Lagos", "Port Louis", "Abuja"], 
     correct: 1,
     funFact: "You just missed her, but you're on the right track. The phone you found has valuable info on it, give us some time and we'll see what intel we can gather from it. ",
@@ -14,14 +19,14 @@ var questionsList = [{
     correctCity: "Ouagadougou"
 }, 
 {
-    question: "We got some intel from the phone, it says here that she's heading to the biggest beach city (and possibly the capital) of Kenya, follow her!",
+    question: "We got some intel from the phone, it says here that 'X' is heading to the biggest beach city (and possibly the capital) of Kenya, follow her!",
     answers: ["Kampala", "Mombasa", "Dakar", "Pretoria"],
     correct: 2,
-    funFact: "How did you miss her? She was so close!",
+    funFact: "Oh man! That was her! She's good, but along the way she left a notebook, scan it and send it to us, our team will look into it.",
     image: "assets/images/mombasa.jpg",
     correctCity: "Mombasa"
 }, 
-{   question: "It looks like she's heading to Asia near the Caucasus bordering Russia, do you know where you're going?",
+{   question: "Turns out she has a rendezvous in Asia. She's in the Caucasus bordering Russia, do you know where you're going?",
     answers: ["Tirana", "Sarajevo", "Tbilisi", "Lubljana"],
     correct: 3,
     funFact: "Well at least she thinks she lost you, keep on her tail, be patient. We'll get her.",
@@ -36,7 +41,7 @@ var questionsList = [{
     correctCity: "Nicaragua"
 },
 {   
-    question: "Okay! She's leaving for her job in the Negev Desert, where will she be flying to get there?",
+    question: "Okay! Her job is in a city near the Negev Desert, what city do you want us to buy your flight to?",
     answers: ["Jerusalem", "Salt Lake City", "Algiers", "Ulaanbaatar"],
     correct: 1, 
     funFact: "Well you set the trap, our team will take care of the rest. Come back to HQ and we'll brief you when you arrive.",
@@ -45,17 +50,16 @@ var questionsList = [{
 
 }];
 
-// FROM HERE ---- MY TIMER I SHOULD TURN THIS INTO AN OBJECT. //
+// FROM HERE ---- MY TIMER: I THINK I SHOULD TURN THIS INTO AN OBJECT. //
 
 var intervalId; 
-var number = 11; 
+var number = 31; 
 
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    console.log("THIS IS RUNNING");
-    number = 11; 
+    number = 31; 
 }
 
 function decrement() {
@@ -79,13 +83,18 @@ var waitForList;
 function gameOver() {
     $("#photo-slot").html(" ");
     $("#timer").html("Well, Agent. We got the report back. Here's how you did."); 
-    $("#question").html("You Got " + correct + " Correct");
-    $("#button-1").remove(); 
-    $("#button-2").html("You Got " + incorrect + " Wrong")
-    $("#button-3").remove(); 
-    $("button-4").remove(); 
+    $("#photo-slot").html("You Got " + correct + " Right and " + incorrect + " Wrong");
+    $("#question").html("<h1>ANALYSIS: </h1>");
+    $(".guess-button").remove(); 
     stop();
-};
+
+    if (correct > incorrect) {
+        $("#photo-slot2").html("<img src=" + missionResult[0].win + ">")
+    } else { $("#photo-slot2").html("<img src=" + missionResult[0].lose + ">")
+    }
+
+
+}
 
 function ifCorrect() {
     $("#question").html(questionsList[aQQ-1].funFact);
@@ -106,7 +115,7 @@ function ifWrong() {
     //show headliner as wrong! 
     $("#timer").html("WRONG! The correct answer is " + questionsList[aQQ-1].correctCity); 
     incorrect++; 
-    waitForList = setTimeout(addQuestions, 5000);
+    waitForList = setTimeout(addQuestions, 7000);
 }
 
 // variable to count position of questions
@@ -116,6 +125,7 @@ function addQuestions() {
     if (aQQ < questionsList.length) {
         run();
         $("#photo-slot").html("");
+        //could probably turn this into a loop. 
         $("#question").html(questionsList[aQQ].question);
         $("#button-1").html(questionsList[aQQ].answers[0]);
         $("#button-2").html(questionsList[aQQ].answers[1]);
